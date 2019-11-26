8c19ef6b017265a93fca9bbb0801d70ed2b1dd2d
Gurpal Singh Jabble <gj302@itl308.student.eecs.qmul.ac.uk>
2019-11-26 20:32:04 +0000

    static settings

--- project/settings.py
+++ project/settings.py
@@ -113,10 +113,11 @@ USE_TZ = True
 
 LOGIN_URL = '/auction/login/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'auction/static/')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'auction/static/')
+# ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'auction/static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
