efe09c48964a95133e71dd5d2bab96371dffd990
gjabble <gurpsjabble@gmail.com>
2019-11-29 00:24:34 +0000

    updating settings py and wsgi

--- project/settings.py
+++ project/settings.py
@@ -117,12 +117,12 @@ LOGIN_URL = '/auction/login/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
